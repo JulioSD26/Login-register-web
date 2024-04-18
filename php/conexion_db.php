b5fe7c4967d261f2a9190f0e72c8cf27a166a4f3
Julio <juliosanchezd26@gmail.com>
2024-04-17 23:57:17 -0700

    Se agrego la conexion a la bd

---
+++ php/conexion_db.php
@@ -0,0 +1,11 @@
+<?php
+
+    $conexion = mysqli_connect("localhost", "root", "", "login_register_db");
+
+    if($conexion){
+        echo 'Conexión exitosa';
+    } else {
+        echo 'Error al conectar a la base de datos';
+    }
+
+?>
