4e55c8984910a10ada27c60337fa1d4e2cc9756d
Julio <juliosanchezd26@gmail.com>
2024-04-18 15:37:49 -0700

    validacion de login_usuario_db.php

---
+++ php/login_usuario_db.php
@@ -0,0 +1,22 @@
+<?php
+
+    include 'conexion_db.php';
+
+    $correo = $_POST['correo'];
+    $contrasena = $_POST['contrasena'];
+
+    $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios 
+    WHERE correo = '$correo' AND contrasena = '$contrasena'");
+
+    if(mysqli_num_rows($validar_login) > 0){
+        header("location: ../bienvenido.php");
+        exit();
+    } else {
+        echo '<script>
+            alert("Usuario no encontrado, por favor verifique los datos introducidos");
+            window.location = "../index.php";
+            </script>';
+        exit();
+    }
+
+?>
